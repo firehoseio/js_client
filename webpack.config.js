1d49370e31f8fe937c8a61e7bb5cfbf25a855fac
John Nguyen <hello@jnguyen.me>
2017-10-09 11:03:41 -0700

    Update webpack with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,45 @@
-require('coffee-script/register')
-module.exports = require('./webpack.config.coffee')
+const path         = require("path")
+const webpack      = require("webpack")
+const version      = require("./package.json").version
+const codeName     = require("./package.json")
+const isProduction = process.argv.indexOf("-p") !== -1
+
+module.exports = {
+  entry: {
+    firehose: path.join(__dirname, "lib", "index.js")
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: `[name]${isProduction ? ".min" : ""}.js`,
+    library: "Firehose",
+    libraryTarget: "umd"
+  },
+
+  devtool: "source-map",
+
+  plugins: [
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(version),
+      __CODE_NAME__: JSON.stringify(codeName)
+    })
+  ],
+
+  resolve: {
+    modules: ["node_modules"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".js", ".coffee"]
+  }
+};

fdf0caa9c6120b0feda8c6f111843e55556bdd09
Adam Heath <adam@polleverywhere.com>
2017-10-27 10:18:22 +0800

    Remove unnecessary directory

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,19 @@
-module.exports = require("./firehose")
+import Consumer from "./consumer"
+import LongPollTransport from "./consumer"
+import MultiplexedConsumer from "./consumer"
+import MultiplexedLongPoll from "./consumer"
+import Transport from "./consumer"
+import WebSocketTransport from "./consumer"
+import MultiplexedWebSocket from "./consumer"
+
+export default {
+  version: __VERSION__,
+  codeName: __CODE_NAME__,
+  Consumer: Consumer,
+  LongPollTransport: LongPollTransport,
+  MultiplexedConsumer: MultiplexedConsumer,
+  MultiplexedLongPoll: MultiplexedLongPoll,
+  Transport: Transport,
+  WebSocketTransport: WebSocketTransport,
+  MultiplexedWebSocket: MultiplexedWebSocket
+};
